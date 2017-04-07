f0ca0a711ecede3ea061175efec266e7bc4811ac
Mike Abrahall <mike@abrahall.id.au>
2017-04-07 16:38:35 +1000

    Consider Nmap 0 hosts scanned error as 0 active hosts, change how check_all to respond with a list

--- src/__init__.py
+++ src/__init__.py
@@ -16,15 +16,19 @@ class scanner(object):
         # -n tells Nmap not to reverse resolve the hostname
         #    We've already forward resolved it and it was causing confusion in cases of "music->192.168.10.97->media.lan"
         if 'error' in results['nmap']['scaninfo']:
-            errors = '\n'.join(results['nmap']['scaninfo']['error']).split('\n')
+            errors = set('\n'.join(results['nmap']['scaninfo']['error']).split('\n'))
             # The Nmap library seems to try and split errors by line into a list,
             # but it also seems to be failing some times, so I'm undoing that and doing it myself.
+            print(' '.join(errors),end=': ')
             for err in errors:
-                if not err.startswith('Failed to resolve') and not '' == err:
-                     raise NmapError(errors)
-        self.last_results = results
-        return int(results['nmap']['scanstats']['uphosts'])
-    def active_hosts(self):
+                if err.startswith('Failed to resolve') or '' == err:
+                     pass
+                elif "WARNING: No targets were specified, so 0 hosts scanned." == err:
+                     # Nmap gives this response when it fails to resolve all the supplied DNS names.
+                     # This is not a problem and really just means none of the devices are on the network.
+                     return []
+                else:
+                     raise NmapError(err)
         return [host['hostname'] if host['hostname'] else host['addresses']['ipv4'] for host in self.last_results['scan'].values()]
 
 if '__main__' == __name__:
@@ -33,7 +37,11 @@ if '__main__' == __name__:
     s = scanner(sys.argv[1])
     with open('output.log', 'w') as f:
         while True:
-            string = ' '.join([time.asctime(), 'found', str(s.check_all()), 'active hosts:', str(s.active_hosts())])
+            result = s.check_all()
+            string = ' '.join([time.asctime(), 'found', str(len(result)), 'active hosts:', str(result)])
             f.write(string+'\n')
             print(string)
-            time.sleep(300) # 5 minutes
+            if not result:
+                time.sleep(60)
+            else:
+                time.sleep(300) # 5 minutes
